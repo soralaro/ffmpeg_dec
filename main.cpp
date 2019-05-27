809ba14bccd5e351754a0445771dc6d4c3168be6
root <zhenxiongchen@deepglint.com>
2019-05-27 15:35:31 +0800

    output jpg

--- main.cpp
+++ main.cpp
@@ -9,11 +9,30 @@ extern "C" {
 #include <libavutil/file.h>
 #include <libavutil/imgutils.h>
 }
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+void NV12_2_JPG(unsigned char *Ydata,unsigned char *Udata,unsigned char *Vdata, int width,int height,std::string out_file_name)
+{
+	unsigned char *img_nv21=new unsigned char[width*height*3/2];
+	memcpy(img_nv21,Ydata,width*height);
+	memcpy(img_nv21+width*height,Udata,width*height/4);
+	memcpy(img_nv21+width*height*5/4,Vdata,width*height/4);
+    cv::Mat dst(height,width,CV_8UC3);
+    cv::Mat src(height + height/2,width,CV_8UC1,img_nv21);
+    cv::cvtColor(src,dst,CV_YUV2BGR_I420);
+    std::vector<int> param = std::vector<int>(2);
+    param[0]=CV_IMWRITE_JPEG_QUALITY;
+    param[1]=100;//default(95) 0-100
+    cv::imwrite(out_file_name+".jpg",dst,param);
+    delete [] img_nv21;
+
+}
 
 int main(int argc, char* argv[])
 {
 	printf("star!\n");
-	char* filename = "/projects/video/t1019.h264";
+	char* filename = "./haiguan.h264";
 	AVFormatContext* FormatContext=0;
 	AVCodec* codec=0;
 	AVCodecContext* CodecContext=0;
@@ -92,9 +111,13 @@ int main(int argc, char* argv[])
 	                ret= sws_scale(swsc,(const uint8_t* const*)frame->data, frame->linesize, 0, frame->height,(uint8_t* const*)frameYUV->data, frameYUV->linesize);
 	                if(ret != 0)
 	                {
-	                    fwrite(frameYUV->data[0], CodecContext->width*CodecContext->height, 1, fp);
-	                    fwrite(frameYUV->data[1], CodecContext->width*CodecContext->height/4, 1, fp);
-	                    fwrite(frameYUV->data[2], CodecContext->width*CodecContext->height/4, 1, fp);
+	                	std::string out_file_name="./out_jpg/"+std::to_string(CodecContext->frame_number);
+	                	NV12_2_JPG(frameYUV->data[0],frameYUV->data[1],frameYUV->data[2],CodecContext->width,CodecContext->height,
+	                		out_file_name);
+	                    //fwrite(frameYUV->data[0], CodecContext->width*CodecContext->height, 1, fp);
+	                    //fwrite(frameYUV->data[1], CodecContext->width*CodecContext->height/4, 1, fp);
+	                    //fwrite(frameYUV->data[2], CodecContext->width*CodecContext->height/4, 1, fp);
+
 	                    printf( "write frame_number %d \n", CodecContext->frame_number);
 	                }
 	            }
